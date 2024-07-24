0edbd4d6f2507a361631f5805b70fd42c2a19b5d
Mateusz Gostański <mateusz.gostanski@gmail.com>
2024-07-24 21:55:12 +0200

    :zap: feat: add rules to ts config

--- src/languages/typescript.ts
+++ src/languages/typescript.ts
@@ -1,17 +1,37 @@
 import type { FactoryConfig, TypescriptConfig } from "../types";
-import parserTs from '@typescript-eslint/parser'
-import pluginTs from '@typescript-eslint/eslint-plugin'
+import pluginTs from '@typescript-eslint/eslint-plugin';
+import parserTs from '@typescript-eslint/parser';
+
 
 export const typescriptConfig: FactoryConfig<TypescriptConfig> = (config) => ({
-  name: 'languages/typescript',
+  name: "languages/typescript",
   files: config.files,
   languageOptions: {
     parser: parserTs,
   },
   plugins: {
-    '@typescript-eslint': pluginTs as any,
+    "@typescript-eslint": pluginTs as any,
   },
   rules: {
     ...pluginTs.configs.recommended.rules,
-  }
+    "default-param-last": "off",
+    "@typescript-eslint/default-param-last": "error",
+    "@typescript-eslint/consistent-type-imports": "error",
+    "@typescript-eslint/consistent-type-exports": "error",
+    "@typescript-eslint/explicit-member-accessibility": "error",
+    "@typescript-eslint/no-unnecessary-qualifier": "error",
+    "@typescript-eslint/array-type": "error",
+    "@typescript-eslint/return-await": "error",
+    "@typescript-eslint/prefer-function-type": "error",
+    "prefer-destructuring": "off",
+    "@typescript-eslint/prefer-destructuring": "warn",
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": "error",
+    "@typescript-eslint/no-namespace": "off",
+    "no-magic-numbers": "off",
+    "@typescript-eslint/no-magic-numbers": "warn",
+    "@typescript-eslint/explicit-module-boundary-types": "error",
+    "prefer-promise-reject-errors": "off",
+    "@typescript-eslint/prefer-promise-reject-errors": "error",
+  },
 })
