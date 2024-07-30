8ebe35b36a8c87336a35f9468a730ab6ffd2972e
Mateusz Gostański <mateusz.gostanski@gmail.com>
2024-07-30 21:35:04 +0200

    :pencil2: fix: type problems

--- src/common/prettier.ts
+++ src/common/prettier.ts
@@ -1,7 +1,8 @@
 import { FactoryConfig } from "../types"
+import { Linter } from "eslint"
 import officialPrettierConfig from "eslint-plugin-prettier/recommended"
 
 export const prettierConfig: FactoryConfig = () => ({
   name: "common/prettier",
-  ...officialPrettierConfig,
+  ...(officialPrettierConfig as Linter.FlatConfig),
 })
--- src/frameworks/playwright.ts
+++ src/frameworks/playwright.ts
@@ -1,19 +1,28 @@
 import type { FactoryConfig, PlaywrightConfig } from "../types"
 import playwright from "eslint-plugin-playwright"
+import type { PartialDeep } from "type-fest"
 
-export const playwrightConfig: FactoryConfig<PlaywrightConfig> = (config) => ({
-  name: "frameworks/playwright",
-  files: config.files,
-  ...playwright.configs["flat/recommended"],
-  rules: {
-    "playwright/no-duplicate-hooks": "error",
-    "playwright/no-get-by-title": "error",
-    "playwright/prefer-comparison-matcher": "error",
-    "playwright/prefer-equality-matcher": "error",
-    "playwright/prefer-hooks-in-order": "error",
-    "playwright/prefer-hooks-on-top": "error",
-    "playwright/prefer-strict-equal": "error",
-    "playwright/prefer-to-be": "error",
-    "playwright/prefer-to-contain": "error",
-  },
-})
+export const playwrightConfig: FactoryConfig<PartialDeep<PlaywrightConfig>> = (
+  config
+) => {
+  if (!config) {
+    throw new Error("Playwright config requires a config object")
+  }
+
+  return {
+    name: "frameworks/playwright",
+    files: config.files,
+    ...playwright.configs["flat/recommended"],
+    rules: {
+      "playwright/no-duplicate-hooks": "error",
+      "playwright/no-get-by-title": "error",
+      "playwright/prefer-comparison-matcher": "error",
+      "playwright/prefer-equality-matcher": "error",
+      "playwright/prefer-hooks-in-order": "error",
+      "playwright/prefer-hooks-on-top": "error",
+      "playwright/prefer-strict-equal": "error",
+      "playwright/prefer-to-be": "error",
+      "playwright/prefer-to-contain": "error",
+    },
+  }
+}
